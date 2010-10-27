56a2a96dde4b91e6a24b35fd5128b9979c2ecaff
Alex Brandt <alunduil@alunduil.com>
2010-10-27 15:07:51 -0500

    Proper skeleton for povgen.
    Also have added the first item from the povray tutorial.

--- utilities/map/povgen/lib/povgen.rb
+++ utilities/map/povgen/lib/povgen.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/ruby
 
 ############################################################################
-#    Copyright (C) 2010 by Alex Brandt   #
-#    alunduil@alunduil.com   #
+#    Copyright (C) 2010 by Alex Brandt <alunduil@alunduil.com>             #
 #                                                                          #
 #    This program is free software; you can redistribute it and#or modify  #
 #    it under the terms of the GNU General Public License as published by  #
@@ -20,21 +19,32 @@
 #    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ############################################################################
 
-require 'xircd_plugin'
+require 'trollop'
 
-class HelloWorldBot < XircdPlugin::XircdPlugin
-  attr_accessor :handlers
-end
+module PovGen
+  class PovGenApplication
+    
+    def initialize
+      self.parse_options
+    end
 
-class HelloWorldBotFactory < XircdPlugin::XircdPluginFactory
-INFO=<<INFO
-name: helloworld
-author: Alex Brandt
-description: A simple hello world plugin for xircd.
-INFO
+    def parse_options
+      @options = Trollop::options do
+        version "povgen 1.0.0 (c) 2010 Alex Brandt <alunduil@alunduil.com>"
+        banner "usage: povgen [options]"
 
-  def create(config)
-    HelloWorldBot.new(config)
+        opt :debug, "Output debugging information", :default => false, :short => "-d"
+        opt :verbose, "Output verbose information", :default => false, :short => "-v"
+      end
+
+      @options[:verbose] = @options[:debug] if @options[:debug]
+    end
+
+    def run
+    end
+  end
+
+  class XircdApplicationError < StandardError
   end
 end
 
